1a5d7ba56c07c03bc274b38661e0fee1855d740b
Bobby Murphy <Bobbyjoemurphy@hotmail.co.uk>
2022-08-30 08:23:28 +0100

    1, created basic Clean architecture file structure. 2, Files for retrofit/calling DTOS 3, Created functions to strip the DTO's for information that we can call as a smaller object. 4,added getitemusecase, invokes flow emits loading success else catches errors from http or IO

--- RiverIslandApplication/app/src/main/java/com/example/riverislandapplication/Domain/UseCase/GetItem/GetItemUseCase.kt
+++ RiverIslandApplication/app/src/main/java/com/example/riverislandapplication/Domain/UseCase/GetItem/GetItemUseCase.kt
@@ -1,4 +1,29 @@
 package com.example.riverislandapplication.Domain.UseCase.GetItem
 
-class GetItemUseCase {
+import com.example.riverislandapplication.Common.Resource
+import com.example.riverislandapplication.Data.remote.Dto.toItem
+import com.example.riverislandapplication.Domain.Models.Item
+import com.example.riverislandapplication.Domain.Repository.ItemRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetItemUseCase @Inject constructor(
+    private val repository: ItemRepository
+){
+    operator fun invoke(): Flow<Resource<List<Item>>> = flow{
+        try {
+            emit(Resource.Loading())
+            val items = repository.getitems().map { it.toItem() }
+            emit(Resource.Success(items))
+        }catch (e : HttpException){
+            emit(Resource.Error(e.localizedMessage?:"error occurred"))
+        }catch (e:IOException){
+            emit(Resource.Error("Couldn't reach server. Check internet connection"))
+
+        }
+
+    }
 }
